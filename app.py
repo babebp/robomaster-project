b96d5411e193bc88722f6ee92c45fc19c9f64124
babebp <tpacharawut@hotmail.com>
2022-09-30 15:49:24 +0700

    update main function

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ class MyRobot:
         self.ep_chassis = self.ep_robot.chassis
         self.ep_sensor_adaptor = self.ep_robot.sensor_adaptor
         self.ep_gripper = self.ep_robot.gripper
-        self.delay = time.sleep(0.25)
+        self.delay = 1
         self.x_val = 0.1
         self.y_val = 0.1
         self.distance = 1000
+        self.speed = 30
 
     def read_sensor(self):
         global fl 
@@ -26,6 +27,7 @@ class MyRobot:
         fr = self.ep_sensor_adaptor.get_io(id=1, port=2)
         bl = self.ep_sensor_adaptor.get_io(id=2, port=1)
         br = self.ep_sensor_adaptor.get_io(id=2, port=2)
+        print(f'br: {br}, bl: {bl}, fl: {fl}, fr: {fr}, distance: {robott.distance}')
         
     def sub_data_handler(self, sub_info):
         self.distance = sub_info[0]
@@ -41,37 +43,48 @@ class MyRobot:
         self.ep_servo.moveto(index=2, angle= 0).wait_for_completed()
 
     def go_forward(self):
-        self.ep_chassis.move(x = self.x_val, y = 0, z=0, xy_speed=0.1).wait_for_completed()
+        self.ep_chassis.drive_wheels(w1= 30, w2=30, w3=30, w4=30)
+        time.sleep(self.delay)
 
     def turn_right_til_both_front_gone(self):
         while not (fl == 1 and fr == 1) or (br == 0 or bl == 0):
-            self.ep_chassis.move(x=0, y=self.y_val, z=0, xy_speed=0.1).wait_for_completed()
+            self.ep_chassis.drive_wheels(w1=-self.speed, w2=self.speed, w3=-self.speed, w4=self.speed)
+            time.sleep(self.delay)
             self.read_sensor()
             print('1')
+        self.ep_chassis.drive_wheels(w1=0, w2=0, w3=0, w4=0)
 
     def turn_left_til_back_right_gone(self):
         while not (br == 0 or (fl == 1 and fr == 1)):
-            self.ep_chassis.move(x=0, y=self.y_val, z=0, xy_speed=0.1).wait_for_completed()
+            self.ep_chassis.drive_wheels(w1=self.speed, w2=-self.speed, w3=self.speed, w4=-self.speed)
+            time.sleep(self.delay)
             self.read_sensor()
             print('2')
+        self.ep_chassis.drive_wheels(w1=0, w2=0, w3=0, w4=0)
 
     def turn_right_til_back_left_gone(self):
         while not (bl == 0 or (fl == 1 and fr == 1)):
-            self.ep_chassis.move(x=0, y=self.y_val, z=0, xy_speed=0.1).wait_for_completed()
+            self.ep_chassis.drive_wheels(w1=-self.speed, w2=self.speed, w3=-self.speed, w4=self.speed)
+            time.sleep(self.delay)
             self.read_sensor()
             print('3')
+        self.ep_chassis.drive_wheels(w1=0, w2=0, w3=0, w4=0)
 
     def turn_left_til_front_right_gone(self):
         while not ((fr == 1 and fl == 1) or (br == 0 or bl == 0)):
-            self.ep_chassis.move(x=0, y=-self.y_val, z=0, xy_speed=0.1).wait_for_completed()
+            self.ep_chassis.drive_wheels(w1=self.speed, w2=-self.speed, w3=self.speed, w4=-self.speed)
+            time.sleep(self.delay)
             self.read_sensor()
             print('4')
+        self.ep_chassis.drive_wheels(w1=0, w2=0, w3=0, w4=0)
 
     def turn_right_til_front_left_gone(self):
         while not ((fr == 1 and fl == 1) or (br == 0 or bl == 0)):
-            self.ep_chassis.move(x=0, y=self.y_val, z=0, xy_speed=0.1).wait_for_completed()
+            self.ep_chassis.drive_wheels(w1=-self.speed, w2=self.speed, w3=-self.speed, w4=self.speed)
+            time.sleep(self.delay)
             self.read_sensor()
             print('5')
+        self.ep_chassis.drive_wheels(w1=0, w2=0, w3=0, w4=0)
 
     def pickup(self):
 
@@ -101,5 +114,3 @@ if __name__ == '__main__':
         robott.go_forward()
         pass
         if robott.distance <= 6.2: robott.pickup()  
-
-        print(f'br: {br}, bl: {bl}, fl: {fl}, fr: {fr}, distance: {robott.distance}')
